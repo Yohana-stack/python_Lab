42ad7ee658cc5e419ee98a1852a6ffc7e58bae35
Yohana-stack <omarabdulgadiryohana@gmail.com>
2026-09-11 23:18:19 -0900

    Add files via upload

---
+++ python-week-1-assignment.py
@@ -0,0 +1,11 @@
+# This program introduces me using variables and print statements.
+
+name = "Yohana Abdulgadir Omar"
+age = 20
+favourite_language = "Python"
+goal = "To improve my programming skills and become a better developer."
+
+print("Hello! My name is", name)
+print("I am", age, "years old.")
+print("My favourite programming language is", favourite_language)
+print("My goal for this course is", goal)
